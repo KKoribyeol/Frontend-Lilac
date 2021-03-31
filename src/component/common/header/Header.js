c31c42f68d51adad464735c4bad8f50bd5b71676
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-31 19:04:11 +0900

    Create every page headers.

--- src/component/common/header/Header.js
+++ src/component/common/header/Header.js
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom"
-import "./Common.css"
+import "../Common.css"
 import "./Header.css"
 
 const Header = () => {
     return (
         <div className="header">
-            <div className="header-left">
-                <div className="main-logo"></div>
-                <span className="main-project-name">꼬리별</span>
+            <div className="header-left sort-center">
+                <Link to="/" className="main-link sort-center">
+                    <div className="main-logo"></div>
+                    <span className="main-project-name sort-center">꼬리별</span>
+                </Link>
             </div>
-            <div className="header-middle">
+            <div className="header-middle sort-center">
 
             </div>
-            <div className="header-right">
+            <div className="header-right sort-center">
                 <span className="sign-in">Sign in</span>
-                <Link to="./SignUp">
+                <Link to="/signup">
                     <span className="sign-up">Sign up</span>
                 </Link>
             </div>
