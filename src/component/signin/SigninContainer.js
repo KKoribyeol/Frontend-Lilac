3f42e32d787813468a90a332318a71f9295310f8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-31 20:03:33 +0900

    Add page that signup.

---
+++ src/component/signin/SigninContainer.js
@@ -0,0 +1,14 @@
+import Header from "../common/header/Header";
+import Signup from "./Signin";
+import Signin from "./Signin";
+
+const SigninContainer = () => {
+    return (
+        <>
+            <Header />
+            <Signin />
+        </>
+    )
+}
+
+export default SigninContainer;
