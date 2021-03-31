8f974fcf132a1da2d373e54f5b9f74f6f1d5600c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-31 23:04:21 +0900

    Add page that Login.

--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -1,16 +1,17 @@
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import MainContainer from "../component/main/MainContainer";
-import SignupContainer from "../component/signup/SignupContainer";
+import SignUpContainer from "../component/signup/SignupContainer";
+import SignInContainer from "../component/signin/SignInContainer";
 import React from "react";
 
 const MainRouter = () => {
     return (
         <Router>
             <Route exact path="/" component={MainContainer} />
-            <Route exact path="/signup" component={SignupContainer} />
-            <Route exact path="/signin" component={SignupContainer} />
+            <Route exact path="/signup" component={SignUpContainer} />
+            <Route exact path="/signin" component={SignInContainer} />
         </Router>
     );
 }
 
-export default MainRouter
+export default MainRouter;
