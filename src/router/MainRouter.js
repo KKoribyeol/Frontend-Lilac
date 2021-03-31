e49d053e889b5942bd1fb14060d02e45c26081f5
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-03-31 23:46:31 +0900

    Modify Routing.

--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -3,6 +3,9 @@ import MainContainer from "../component/main/MainContainer";
 import SignUpContainer from "../component/signup/SignupContainer";
 import SignInContainer from "../component/signin/SignInContainer";
 import TargetContainer from "../component/target/TargetContainer";
+import GroupContainer from "../component/group/GroupContainer";
+import TemplateContainer from "../component/template/TemplateContainer";
+import DocumentContainer from "../component/document/DocumentContainer";
 import React from "react";
 
 const MainRouter = () => {
@@ -12,6 +15,9 @@ const MainRouter = () => {
             <Route exact path="/signup" component={SignUpContainer} />
             <Route exact path="/signin" component={SignInContainer} />
             <Route exact path="/target" component={TargetContainer} />
+            <Route exact path="/group" component={GroupContainer} />
+            <Route exact path="/template" component={TemplateContainer} />
+            <Route exact path="/document" component={DocumentContainer} />
         </Router>
     );
 }
