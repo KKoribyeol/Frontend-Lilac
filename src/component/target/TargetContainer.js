a3a31fbe051825a558015e3377ddecec80abae8c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-01 14:45:42 +0900

    Add page about target.

--- src/component/target/TargetContainer.js
+++ src/component/target/TargetContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from "../common/header/Header";
 import Target from "./Target";
+import SideBar from "../common/side-bar/SideBar";
 
 const TargetContainer = () => {
     return (
         <>
             <Header isLoginBefore={false} />
-            <Target/>
+            <>
+                <SideBar/>
+                <Target/>
+            </>
         </>
     );
 };
