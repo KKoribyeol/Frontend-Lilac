be1e1530e8c527bb976b921e4935c75420ee51d8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-02 19:17:21 +0900

    Add feature that project insert.

---
+++ src/component/project/ProjectInsertion.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import "./Project.css"
+import axios from "axios";
+
+const ProjectInsertion = () => {
+    const [ name, setName ] = useState("");
+    const [ description, setDescription ] = useState("");
+    const [ problemMessage, setProblemMessage ] = useState("")
+
+    const handleName = name => setName(name.currentTarget.value);
+    const handleDescription = description => setDescription(description.currentTarget.value);
+
+    const submit = () => {
+        axios.post("http://localhost:6180/project", {
+            name: name,
+            description: description,
+        }, {
+            headers: {
+                Authorization: localStorage.getItem("accessToken"),
+            },
+        }).then(result => {
+            window.location.reload();
+        }).catch(error => {
+            switch (error.response.data.code) {
+                case "ALREADY_EXIST_PROJECT":
+                    setProblemMessage("다시 한 번 프로젝트를 생성해주세요.");
+                    break;
+                default:
+                    alert(error.response.data.message);
+                    break;
+            }
+        })
+    }
+
+    return (
+        <div className="content-wrapper-wrapper">
+            <div className="content-wrapper">
+                <div className="project-content-name-wrapper project-void">
+                    <input
+                        type="text"
+                        onChange={handleName}
+                        value={name}
+                        className="project-input-tag"
+                    />
+                </div>
+                <div className="project-content-description-wrapper">
+                    <input
+                        type="text"
+                        onChange={handleDescription}
+                        value={description}
+                        className="project-input-tag"
+                    />
+                </div>
+            </div>
+            <div className="project-insertion-image" onClick={submit}/>
+        </div>
+    );
+};
+
+export default ProjectInsertion;
