14ebd78333b49f203aec27ab03158fb23818a316
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-04 22:48:11 +0900

    create feature that target.

--- src/component/target/TargetContent.js
+++ src/component/target/TargetContent.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import "./Target.css"
+import "../common/Common.css"
 
-const TargetContent = () => {
+const TargetContent = ({ token, nickname, name }) => {
+
+
     return (
-        <div>
-            
+        <div className="content-wrapper-wrapper">
+            <div className="content-wrapper">
+                <div className="target-content-token-wrapper">
+                    <span className="target-content">{token}</span>
+                </div>
+                <div className="target-content-nickname-wrapper">
+                    <span className="target-content">{token}</span>
+                </div>
+                <div className="target-content-name-wrapper">
+                    <span className="target-content">{token}</span>
+                </div>
+            </div>
         </div>
     );
 };
