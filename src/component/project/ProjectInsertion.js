11714d752474050ade146dc987d1d7052b99a3e3
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-04 22:46:57 +0900

    create insertion that project.

--- src/component/project/ProjectInsertion.js
+++ src/component/project/ProjectInsertion.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import "./Project.css"
+import "../common/Common.css"
 import axios from "axios";
 import * as config from "../../config";
 
 const ProjectInsertion = () => {
     const [ name, setName ] = useState("");
     const [ description, setDescription ] = useState("");
-    const [ problemMessage, setProblemMessage ] = useState("")
 
     const handleName = name => setName(name.currentTarget.value);
     const handleDescription = description => setDescription(description.currentTarget.value);
@@ -24,7 +24,7 @@ const ProjectInsertion = () => {
         }).catch(error => {
             switch (error.response.data.code) {
                 case "ALREADY_EXIST_PROJECT":
-                    setProblemMessage("다시 한 번 프로젝트를 생성해주세요.");
+                    alert("다시 한 번 프로젝트를 생성해주세요.");
                     break;
                 default:
                     alert(error.response.data.message);
@@ -53,7 +53,7 @@ const ProjectInsertion = () => {
                     />
                 </div>
             </div>
-            <div className="project-insertion-image" onClick={submit}/>
+            <div className="content-insertion-image" onClick={submit}/>
         </div>
     );
 };
