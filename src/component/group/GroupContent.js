a0eee195e60f7eb82cbf7e065e63cd0d5f22c284
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 20:53:14 +0900

    create page that group.

--- src/component/group/GroupContent.js
+++ src/component/group/GroupContent.js
@@ -15,6 +15,8 @@ const GroupContent = ({ name }) => {
     const [ modificationName, setModificationName ] = useState("");
     const [ modificationMode, setModificationMode ] = useState(false);
 
+    const [ groupingTargetToken, setGroupingTargetToken ] = useState("");
+
     const switchModificationMode = () => {
         setModificationMode(!modificationMode);
     }
@@ -39,6 +41,8 @@ const GroupContent = ({ name }) => {
     }
 
     const handleName = name => setModificationName(name.currentTarget.value);
+
+    const handleGroupingTargetToken = token => setGroupingTargetToken(token.currentTarget.value);
 
     const modifyGroup = () => {
         axios.patch(config.BASE_URL + `/group/${name}`, {
@@ -93,6 +97,60 @@ const GroupContent = ({ name }) => {
         })
     }
 
+    const ungroupTarget = token => {
+        axios.delete(config.BASE_URL + `/affiliation?group-name=${name}&target-token=${token}`, {
+            headers: {
+                Authorization: localStorage.getItem("accessToken"),
+                projectCode: localStorage.getItem("projectCode"),
+            }
+        }).then(() => {
+            window.location.reload();
+        }).catch(() => {
+            alert("타겟을 그룹에서 해제하는데 실패하였습니다.");
+        })
+    }
+
+    const submit = () => {
+        axios.post(config.BASE_URL + "/affiliation", {
+            groupName: name,
+            targetToken: groupingTargetToken,
+        }, {
+            headers: {
+                Authorization: localStorage.getItem("accessToken"),
+                projectCode: localStorage.getItem("projectCode"),
+            },
+        }).then(() => {
+            window.location.reload();
+        }).catch(error => {
+            switch (error.response.status) {
+                case 400:
+                    switch (error.response.data.code) {
+                        case "ALREADY_EXIST_AFFILIATION":
+                            alert("이미 존재하는 타겟입니다.");
+                            break;
+                        default:
+                            alert("알 수 없는 오류");
+                            break;
+                    } break;
+                case 404:
+                    switch (error.response.data.code) {
+                        case "TARGET_NOT_FOUND":
+                            alert("타겟을 찾을 수 없습니다.");
+                            break;
+                        case "GROUP_NOT_FOUND":
+                            alert("그룹을 찾을 수 없습니다.");
+                            break;
+                        default:
+                            alert("알 수 없는 오류");
+                            break;
+                    } break;
+                default:
+                    alert("알 수 없는 오류");
+                    break;
+            }
+        })
+    }
+
     return (
         <>
             <div className="content-wrapper-wrapper">
@@ -129,7 +187,23 @@ const GroupContent = ({ name }) => {
                 }
             </div>
             {
-                setDetailMode ?
+                detailMode ?
+                    <div className="group-detail-description-wrapper-wrapper">
+                        <div className="group-detail-description-wrapper">
+                            <div className="target-content-token-wrapper target-void">
+                                <span className="target-content-description">Token</span>
+                            </div>
+                            <div className="target-content-nickname-wrapper">
+                                <span className="target-content-description">Nickname</span>
+                            </div>
+                        </div>
+                    </div>
+                    :
+                    <>
+                    </>
+            }
+            {
+                detailMode ?
                     affiliatedTarget.targets.map(target => {
                         return (
                             <div className="content-wrapper-wrapper">
@@ -141,6 +215,7 @@ const GroupContent = ({ name }) => {
                                         <span className="target-content">{target.nickname}</span>
                                     </div>
                                 </div>
+                                <div className="content-deletion-image" onClick={() => ungroupTarget(target.token)}/>
                             </div>
                         )
                     })
@@ -148,8 +223,27 @@ const GroupContent = ({ name }) => {
                     <>
                     </>
             }
+            {
+                detailMode ?
+                    <div className="content-wrapper-wrapper">
+                        <div className="group-detail-wrapper">
+                            <div className="target-content-token-wrapper target-void">
+                                <input
+                                    type="text"
+                                    onChange={handleGroupingTargetToken}
+                                    value={groupingTargetToken}
+                                    className="group-input-tag"
+                                />
+                            </div>
+                        </div>
+                        <div className="content-insertion-image" onClick={submit}/>
+                    </div>
+                    :
+                    <>
+                    </>
+            }
         </>
     );
-};
+}
 
 export default GroupContent;
