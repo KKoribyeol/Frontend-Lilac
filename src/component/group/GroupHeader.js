f6e8d1bb801f51c0abb917cac672ed0ef1d26810
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 09:43:37 +0900

    create feature that target search, modify, delete.

---
+++ src/component/group/GroupHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Group.css"
+import "../common/Common.css"
+
+const GroupHeader = () => {
+    return (
+        <div className="feature-body-header">
+            <div className="group-header-name-wrapper feature-header-wrapper">
+                <span className="feature-header-font">Name</span>
+            </div>
+        </div>
+    );
+};
+
+export default GroupHeader;
