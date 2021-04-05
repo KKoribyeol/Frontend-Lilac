db746c284f1bd5b321e2aed7a07a8932ae464ec1
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-05 09:43:21 +0900

    create feature that target search, modify, delete.

--- src/component/group/GroupContainer.js
+++ src/component/group/GroupContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from "../common/header/Header";
 import Group from "./Group";
+import SideBar from "../common/side-bar/SideBar";
 
 const GroupContainer = () => {
     return (
         <>
             <Header isLoginBefore={false} />
-            <Group/>
+            <>
+                <SideBar/>
+                <Group/>
+            </>
         </>
     );
 };
