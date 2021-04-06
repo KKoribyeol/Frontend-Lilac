52a459b9c27949d362c08b3ec082310bf414a5a1
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-06 13:18:48 +0900

    create page that api document.

--- src/component/document/DocumentContainer.js
+++ src/component/document/DocumentContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from "../common/header/Header";
 import Document from "./Document";
+import SideBar from "../common/side-bar/SideBar";
 
 const DocumentContainer = () => {
     return (
         <>
             <Header isLoginBefore={false} />
-            <Document/>
+            <>
+                <SideBar/>
+                <Document/>
+            </>
         </>
     );
 };
